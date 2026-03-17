c3a571ac458dfdfaa8dd9c0c8a2321ed8d9709cc
Dima <dmitrynikolaev3@gmail.com>
2026-03-17 21:29:01 +0300

    Добавьте файлы проекта.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1 число: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите 2 число: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Результат сложения: {a + b}");
+        }
+    }
+}
